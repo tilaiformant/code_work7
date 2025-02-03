13247ff52cffee4d6fcfb800f4d2cae948283da4
wujianyu <wujianyu322@gmail.com>
2025-02-03 14:10:02 +0800

    函數

---
+++ test15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void output()
+{
+    printf("output !\n");
+    return;
+}
+int main()
+{
+    output();
+    printf("ch9_1.c\n");
+    output();
+    getchar();
+    return 0;
+}
