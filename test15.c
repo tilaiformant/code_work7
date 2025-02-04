e7059b9674ea0eec212cbdbc184e1c75098e3cb9
wujianyu <wujianyu322@gmail.com>
2025-02-04 12:28:12 +0800

    比較大小函數

--- test15.c
+++ test15.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
-void output()
+
+void larger(int a, int b)
 {
-    printf("output !\n");
-    return;
+    if ( a < b )
+        printf("較大值是 %d \n", b);
+    else if ( a > b)
+        printf("較大值是 %d \n", a);
+    else
+        printf("兩數值相等 \n");
+
 }
 int main()
 {
-    output();
-    printf("ch9_1.c\n");
-    output();
+    int i, j;
+    printf("請輸入兩數值 \n ==> ");
+    scanf("%d %d",&i,&j);
+    larger(i,j);
     getchar();
     return 0;
 }
+
+
+
+
+
+
+    
